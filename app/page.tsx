67a78b0bdd215798a76163c866707970764fe734
mxnan <128995981+mxnan@users.noreply.github.com>
2024-03-01 22:10:29 +0530

    supabase added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function Page() {
   return (
-    <div className="max-w-3xl mx-auto md:py-10 h-screen">
+    <div className="max-w-3xl mx-auto md:py-10 max-md:px-4 max-md:py-2 h-screen">
       <div className="h-full border rounded-md">
         <div className="h-20">
-          <div className="p-5 border-b">
+          <div className="p-5 border-b flex items-center justify-between">
             <div>
               <h1 className="text-xl font-bold">Daily Chat</h1>
               <div className="flex items-center gap-1">
@@ -13,7 +14,7 @@ export default function Page() {
                 <h1 className="text-sm">2 online</h1>
               </div>
             </div>
-            <button></button>
+            <Button>Login</Button>
           </div>
         </div>
       </div>

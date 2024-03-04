e437a351478891e320d89e2611905b55a9bd6cb2
mxnan <128995981+mxnan@users.noreply.github.com>
2024-03-04 14:35:15 +0530

    setup states using zustand

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
-
 import ChatHeader from "@/components/ChatHeader";
+import InitUser from "@/lib/store/InitUser";
 import { supabaseServer } from "@/lib/supabase/server";
 import React from "react";
 
 export default async function Page() {
-
   const supabase = supabaseServer();
 
   const { data } = await supabase.auth.getSession();
- 
 
   return (
-    <div className="max-w-3xl mx-auto md:py-10 max-md:px-4 max-md:py-2 h-screen">
-      <div className="h-full border rounded-md">
-       <ChatHeader user={data.session?.user} />
+    <>
+      <div className="max-w-3xl mx-auto md:py-10 max-md:px-4 max-md:py-2 h-screen">
+        <div className="h-full border rounded-md">
+          <ChatHeader user={data.session?.user} />
+        </div>
       </div>
-    </div>
+      <InitUser user={data.session?.user} />
+    </>
   );
 }
-
-

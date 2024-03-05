9139c4223712cef481f416134bbfb6a39ea9d4cb
mxnan <128995981+mxnan@users.noreply.github.com>
2024-03-05 20:55:01 +0530

    created user and message states

--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -1,16 +1,18 @@
 import React, { Suspense } from "react";
 import ListMessages from "./ListMessages";
 import { supabaseServer } from "@/lib/supabase/server";
+import InitMessages from "@/lib/store/InitMessages";
 
 export default async function ChatMessages() {
   const supabase = supabaseServer();
 
   const { data } = await supabase.from("messages").select("*,users(*)");
 
-  console.log(data)
+  console.log(data);
   return (
     <Suspense fallback={"loading .. "}>
       <ListMessages />
+      <InitMessages messages={data || []} />
     </Suspense>
   );
 }
